f928b466f4a3d77c0a0858c651e64aa87ade8ef5
phamquangviet891 <20522153@gm.uit.edu.vn>
2023-09-21 22:04:31 +0700

    bug: zipkin has no trace

---
+++ department-service/src/main/java/com/quangviet/departmentservice/controller/DepartmentController.java
@@ -0,0 +1,42 @@
+package com.quangviet.departmentservice.controller;
+
+import com.quangviet.departmentservice.model.Department;
+import com.quangviet.departmentservice.repository.DeparmentRepository;
+import org.apache.logging.log4j.spi.LoggerRegistry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/department")
+public class DepartmentController {
+
+    private static final Logger LOGGER
+            = LoggerFactory.getLogger(DepartmentController.class);
+    @Autowired
+    private DeparmentRepository repository;
+
+    @PostMapping
+    public Department add(@RequestBody Department department){
+        LOGGER.info("Department add: {}", department);
+        return repository.addDepartment(department);
+    }
+
+    @GetMapping
+    public List<Department> findAll(){
+        LOGGER.info("Department find");
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Department findById(@PathVariable Long id){
+        LOGGER.info("Department find: id {}",id);
+        return  repository.findById(id);
+    }
+
+
+}
---
+++ department-service/src/main/java/com/quangviet/departmentservice/model/Employee.java
@@ -0,0 +1,5 @@
+package com.quangviet.departmentservice.model;
+
+public record Employee (Long id, Long departmentId, String name, int age, String position){
+
+}
